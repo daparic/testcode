dc54f747ed3a810e13fecff3275ac1b27892b489
Your Name <you@example.com>
2023-04-11 16:27:26 +0800

    init

---
+++ CERT_C-DCL31-a/helper.c
@@ -0,0 +1,4 @@
+
+int addnum(int a, int b) {
+    return a + b;
+}
---
+++ CERT_C-DCL31-a/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int result = addnum(3, 4);
+    printf("result = %d\n", result);
+    return 0;
+}
